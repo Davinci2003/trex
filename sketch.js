76a4b4677d1ccac4c0a16ee7f0d3f19011f49d64
Davinci2003 <67372840+Davinci2003@users.noreply.github.com>
2020-07-07 17:04:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var cloud, obstacles, cloudGroup, obstaclesGroup
+var restart,gameOver,restartImage,gameOverImage;
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png")
+  
+  gameOverImage = loadImage("gameOver.png");
+  restartImage = loadImage("restart.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -2;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+}
+
+function draw() {
+  background(220);
+  
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  drawSprites();
+}
